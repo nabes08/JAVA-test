de7cf0120145b4a517c2e0cb92f0bb8951b0a728
nabes08 <nabesmohammed@gmail.com>
2025-11-05 23:53:12 +0530

    Create Back.java

---
+++ HW/homeworks/Back.java
@@ -0,0 +1,9 @@
+public class Back {
+    public static void main(String[] args) {
+        if (args.length > 0) {
+            System.out.println("Welcome " + args[0]);
+        } else {
+            System.out.println("Please provide a name as a command-line argument.");
+        }
+    }
+}
